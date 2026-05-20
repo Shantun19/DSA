5d0d6e34fd8ca7b119e193f889eb73d864da9951
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2026-05-20 11:28:48 +0530

    Pattern_02

---
+++ 02 Pattern_02.java
@@ -0,0 +1,26 @@
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        printPattern02(5 , 5);
+    }
+    
+    public static void printPattern02(int row , int cols) {
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<cols; j++) {
+                if(j <= i) System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
